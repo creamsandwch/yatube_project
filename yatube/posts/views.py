c64813609f9e355b3df6ec9011b24b547d05c2df
creamsandwch <neuronrus1996@gmail.com>
2022-10-19 16:12:03 +0500

    added template dir, posts/index.html template and modified its view in posts app

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
