6757ca33c8db9d4af026aaabfb67bf015630138d
creamsandwch <neuronrus1996@gmail.com>
2022-10-20 16:22:04 +0500

    modified posts/views.index() to get objects from Post model

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 
 
 def index(request):
-    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request):
