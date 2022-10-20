16aa500a554621631cb1cd5bcf18a78bc606afb2
creamsandwch <neuronrus1996@gmail.com>
2022-10-20 14:58:22 +0500

    added model "Post", migrated it into db

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,16 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    text: str = models.TextField()
+    pub_date: datetime = models.DateTimeField(auto_now_add=True)
+    author: int = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='posts',
+        )
