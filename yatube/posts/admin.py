30bf92ada0f93f425a0b82cbe3d939312220faeb
creamsandwch <neuronrus1996@gmail.com>
2022-10-20 16:03:24 +0500

    empty_value_display added to PostAdmin class

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -9,6 +9,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('text', 'pub_date', 'author')
     search_fields = ('text',)
     list_filter = ('pub_date',)
+    empty_value_display = '-empty-'
 
 
 admin.site.register(Post, PostAdmin)
